f4072277071dc9986389630190e8c5c9c475337e
Alexandr Karensky <skittles8bit@gmail.com>
2021-05-17 14:52:24 +0300

    feat: implementation Two Dimensional Array Sorter function

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,42 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    
+    NSMutableArray *arrStr = [NSMutableArray new];
+    NSMutableArray *arrNum = [NSMutableArray new];
+
+    for (int i = 0; i < array.count; i++) {
+        
+        if ([array[i] isKindOfClass: NSArray.class]) {
+            if ([array[i].firstObject isKindOfClass: NSString.class]) {
+                [arrStr addObjectsFromArray: array[i]];
+            } else if ([array[i].firstObject isKindOfClass: NSNumber.class]) {
+                [arrStr addObjectsFromArray: array[i]];
+            }
+        } else {
+            return @[];
+        }
+        
+    }
+        
+    if (arrNum.count > 0 && arrStr.count > 0) {
+        [arrNum sortUsingDescriptors:
+         [NSArray arrayWithObjects: [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES], nil]];
+        [arrStr sortUsingDescriptors:
+         [NSArray arrayWithObjects: [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:NO], nil]];
+        
+        return @[arrNum, arrStr];
+    } else if (arrNum.count == 0) {
+        [arrStr sortUsingDescriptors:
+         [NSArray arrayWithObjects:[NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES], nil]];
+        
+        return arrStr;
+    } else {
+        [arrNum sortUsingDescriptors:
+         [NSArray arrayWithObjects:[NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES], nil]];
+        
+        return arrNum;
+    }
 }
 
 @end
